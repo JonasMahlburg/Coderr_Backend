07d137fe8f3a9c1613036bacb78abbae343dfd79
JonasMahlburg <darrylmanga@yahoo.com>
2025-08-01 15:53:10 +0200

    fix: update settings

--- up.sh
+++ up.sh
@@ -1,5 +1,5 @@
 git add .
 git commit -m "$*"
 git push
-ssh paintingotter34@35.204.204.97 "cd projects/Coderr_Backend && sudo git pull && sudo supervisorctl restart coderr-gunicorn"
+ssh paintingotter34@34.13.212.51 "cd projects/Coderr_Backend && sudo git pull && sudo supervisorctl restart coderr-gunicorn"
 
